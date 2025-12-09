2ad093beebe945989c7fabe08873b206c3087785
sofsantoro005 <sants329@newschool.edu>
2025-12-09 00:01:48 -0500

    update css and html

---
+++ entry-5/script.js
@@ -0,0 +1,20 @@
+const images = [
+    "../my-images/art/IMG_3568.JPG",
+    "../my-images/fire escapes/fire3.JPG",
+    "../my-images/buildings/IMG_4490.JPG",
+    "../my-images/buildings/IMG_4393.JPG",
+    "../my-images/buildings/IMG_1037.jpeg", 
+  
+
+
+];
+
+// picks random image
+const randomIndex = Math.floor(Math.random() * images.length);
+
+// gets HTML image tag
+const img = document.getElementById("randomImage");
+
+// sets image source
+img.src = images[randomIndex];
+ 
