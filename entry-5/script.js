0eb7b20bf9a07a416611a7c88cef870715876c89
sofsantoro005 <sants329@newschool.edu>
2025-12-09 19:30:27 -0500

    update javascript

--- entry-5/script.js
+++ entry-5/script.js
@@ -2,10 +2,8 @@ const images = [
     "../my-images/art/IMG_3568.JPG",
     "../my-images/fire escapes/fire3.JPG",
     "../my-images/buildings/IMG_4490.JPG",
-    "../my-images/buildings/IMG_4393.JPG",
-    "../my-images/buildings/IMG_1037.jpeg", 
+    "../my-images/buildings/IMG_4393.JPG", 
     "../my-images/art/IMG_5112.JPG", 
-    "../my-images/interiors/IMG_0653.jpeg"
     
 ];
 
